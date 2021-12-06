82b305c78c67dc93d1807381de2119e77a7b19b6
Samuel <samuel.ward@outlook.fr>
2021-12-06 22:44:00 +0100

    update

--- main.py
+++ main.py
@@ -7,17 +7,65 @@ import string
 class MDPGen:
 
     def __init__(self):
-        __config = self.__getConfig()
-        self.__version = __config["version"]
-        self.__authors = __config["authors"]
+        self.__config = self.__getConfig()
+        self.__version = self.__config["version"]
+        self.__authors = self.__config["authors"]
         self.__lettersLower = list(string.ascii_lowercase)
         self.__lettersUpper = list(string.ascii_uppercase)
-        self.__numbers = [str(element) for element in range(0, 10)]
+        self.__numbers = [str(number) for number in range(0, 10)]
         self.__specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"]
+        self.__defaultProfile = self.__config["default"]["profileID"]
+        self.__selectedProfile = self.__config["profiles"][self.__defaultProfile]
 
     def __getConfig(self):
         with open("config.json", "r") as configFile:
             return json.load(configFile)
+
+    def addProfile(self):
+        name = input("Profile name: ")
+        length = input("Password length: ")
+        print("Supported characters:\n\tLowercase Letters (1)\n\tUppercase Letters (2)\n\tNumbers (3)\n\tSpecial Characters (4)")
+
+        charsUnformatted = input(
+            "Password characters (seperate types with ','): ")
+        chars = [mode.strip() for mode in charsUnformatted.split(",")]
+
+        data = {
+            "name": name,
+            "length": length,
+            "chars": chars,
+        }
+        with open("config.json", "r+") as configFile:
+            configFileData = json.load(configFile)
+            configFileData["profiles"].append(data)
+            configFile.seek(0)
+            json.dump(configFileData, configFile, indent=4)
+            print(f"Successfully added profile '{name}'!")
+
+    def showProfiles(self):
+        configFileData = self.__getConfig()
+        for profile in configFileData["profiles"]:
+            print(
+                profile["name"] +
+                " >>> Length: " + profile["length"] +
+                " && Characters: " + str(profile["chars"])
+            )
+
+    def deleteProfile(self):
+        print("Available Profiles:")
+        self.showProfiles()
+        name = input("Profile to delete (enter name): ")
+        configFileData = self.__getConfig()
+        profiles = configFileData["profiles"]
+
+        for i in range(len(profiles)):
+            if profiles[i]["name"] == name:
+                profiles.pop(i)
+                print(f"Successfully deleted profile '{name}'!")
+                break
+
+        with open("config.json", "w") as configFile:
+            configFileData = json.dump(configFileData, configFile, indent=4)
 
     @property
     def version(self):
@@ -47,12 +95,9 @@ class MDPGen:
 def main():
 
     app = MDPGen()
-    print(
-        f"MDPGen v{app.version} developed by {app.authors[0]['name']} & {app.authors[1]['name']}")
-    print(app.numbers)
-    print(app.lettersLower)
-    print(app.lettersUpper)
-    print(app.specialCharacters)
+    print(f"\nMDPGen v{app.version}\n")
+    app.addProfile()
+    app.deleteProfile()
 
 
 if __name__ == "__main__":
